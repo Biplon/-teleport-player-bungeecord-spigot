508b2a5381e9a831caa4529e2d27632f24796ea0
Vestri <p.prokop@firenja.com>
2020-08-01 18:04:44 +0200

    init 2

--- src/tppbs/java/Tppbs.java
+++ src/tppbs/java/Tppbs.java
@@ -1,5 +1,6 @@
 package tppbs.java;
 
+import net.minecraft.server.v1_15_R1.CommandData;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.World;
@@ -14,12 +15,6 @@ import java.io.IOException;
 public class Tppbs extends JavaPlugin implements PluginMessageListener
 {
     public static Tppbs instance;
-
-    public static Tppbs getInstance()
-    {
-        return instance;
-    }
-
 
     @Override
     public void onEnable()
@@ -37,21 +32,24 @@ public class Tppbs extends JavaPlugin implements PluginMessageListener
     @Override
     public void onPluginMessageReceived(String channel, Player player, byte[] message)
     {
-
             DataInputStream msgin = new DataInputStream(new ByteArrayInputStream(message));
             try
             {
                 String c = msgin.readUTF();
-                String somedata = msgin.readUTF(); // Read the data in the same way you wrote it
-                String[] werte = somedata.split(",");
-                Player p = Bukkit.getPlayer(werte[0]);
-                World w = Bukkit.getWorld(werte[4]);
-                float x = Integer.parseInt(werte[1]);
-                float y =  Integer.parseInt(werte[2]);
-                float z =  Integer.parseInt(werte[3]);
-                Location loc = new Location(w, x, y, z);
-                p.teleport(loc);
-                Bukkit.getLogger().info(somedata);
+                if (c.equals("tppbc:main"))
+                {
+                    String somedata = msgin.readUTF();
+                    String[] values = somedata.split(",");
+                    Player p = Bukkit.getPlayer(values[0]);
+                    World w = Bukkit.getWorld(values[4]);
+                    float x = Integer.parseInt(values[1]);
+                    float y =  Integer.parseInt(values[2]);
+                    float z =  Integer.parseInt(values[3]);
+                    Location loc = new Location(w, x, y, z);
+                    assert p != null;
+                    p.teleport(loc);
+                    Bukkit.getLogger().info(somedata);
+                }
             }
             catch (IOException e)
             {
